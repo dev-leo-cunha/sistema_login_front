7fd3da4ce429a3d7e4c7929a7315c1fccf34ef2b
Leonardo Cunha <leonardo.cunha.2203@gmail.com>
2023-06-26 21:00:45 -0300

    Alterando Base URL

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    // baseURL: 'https://tranquil-ocean-04946.herokuapp.com/'
-   baseURL: 'http://localhost:3000/'
+    baseURL: 'https://tranquil-ocean-04946.herokuapp.com/'
+//    baseURL: 'http://localhost:3000/'
 })
 
 export default api
